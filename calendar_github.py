99bd91ad7e3b1bdd143ca85379cdc5bad99612b3
Anna <gafurova.anya@mail.ru>
2021-01-29 00:32:04 +0400

    new commit

--- calendar_github.py
+++ calendar_github.py
@@ -1,45 +1,47 @@
 from datetime import datetime 
 import caldav 
  
+ 
+DTSTART = "20210126T060000Z" 
+DTEND = "20210127T230000Z" 
+ 
+location = "MY OFFICE" 
+ 
 URL = "https://nextcloud05.webo.cloud/remote.php/dav" 
 UserName = "anna.gafurova@htw-dresden.de" 
-Password = " " 
-
-DTSTAMP:"20210126T060000Z"
-DTSTART:"20210126T060000Z" 
-
-#Setting up a caldav client object and a principal object
+Password = "" 
+ 
 client = caldav.DAVClient(url=URL, username=UserName, password=Password) 
 my_principal = client.principal() 
-
-#fetching calendars:
 calendars = my_principal.calendars() 
-
-#find a list of calendars in my principal, print their names and url
+ 
 if calendars: 
     print("your principal has %i calendars:" % len(calendars)) 
     for c in calendars: 
-        print(" Name: %s URL: %s" % (c.name, c.url)) 
+        print(" Name: %-20s URL: %s" % (c.name, c.url)) 
 else: 
     print("your principal has no calendars") 
  
-#creating a calendar:
-my_new_calendar = my_principal.make_calendar(name="Test calendar") 
-
-#Adding an event to the calendar:
+my_new_calendar = my_principal.make_calendar(name="SASeu") 
+ 
+print(my_new_calendar) 
+ 
 my_event = my_new_calendar.save_event("""BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Example Corp.//CalDAV Client//EN
 BEGIN:VEVENT
-UID:20210126T060000Z-123401@example.com 
-DTSTAMP:%s 
-DTSTART:%s 
-DTEND:20210127T230000Z 
+UID:20210126T060000Z-123401@example.com
+DTSTAMP:20210126T060000Z
+DTSTART:%s
+DTEND:%s
 RRULE:FREQ=YEARLY
-LOCATION:My office 
-CATEGORIES:Business 
+LOCATION:%s
+CATEGORIES:Business
 DESCRIPTION:This is a note associated with the meeting
-SUMMARY:Meeting
+SUMMARY:Do the needful
 END:VEVENT
 END:VCALENDAR
-""")
+""" % (DTSTART, DTEND, location))
+
+ 
+print('Hello %s, %s' % (DTSTART, DTEND))
