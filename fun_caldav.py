2aa21c7d96d076e610bda1cb1222f6da0d88e6f4
Anna <gafurova.anya@mail.ru>
2021-01-31 15:49:54 +0400

    commit

--- fun_caldav.py
+++ fun_caldav.py
@@ -19,9 +19,16 @@ def get_data():
 def get_values():  
     data=get_data()
     i=0
+    list=[]
     for value in data:
         print(value[3],value[4],value[5])
+        list.append(value[3]),
+        list.append(value[4])
+        list.append(value[5])
+        "A = {0}, B = {1}, C = {2}".format(*list)
+        
     i=i+1
+    print(list)
 get_values()
 
 
