af989c723b10390a9528d86ccd8f5957a30e8d62
Anna <gafurova.anya@mail.ru>
2021-02-06 15:34:01 +0400

    commit

--- calendar+gui.py
+++ calendar+gui.py
@@ -5,6 +5,7 @@ from icalendar import Calendar, Event
 from pytz import UTC # timezone
 import pytz
 
+#Function for getting data from csv file using file directory
 def get_data_from_scv(csv_path):
     try:
         with open(rf"{csv_path}", encoding="utf8", errors='ignore') as file:
@@ -16,40 +17,51 @@ def get_data_from_scv(csv_path):
     except:
         print("Datei nicht gefunden\n")
 
-          
+#Function to put data from csv into lists: list of dtstart, dtend and summary for calendar       
 def get_values(csv_data):  
     data = csv_data
-    list1=[] 
-    list2=[]
-    list3=[]
+    list_with_dtstart_as_str=[] 
+    list_with_dtend_as_str=[]
+    list_with_summaries=[]
+
     for value in data:
-        list1.append(value[3]+" "+value[4])
-        list2.append(value[3]+" "+value[5])  
-        list3.append(value[7]+','+value[6]+','+ value[8])         
+        list_with_dtstart_as_str.append(value[3]+" "+value[4])
+        list_with_dtend_as_str.append(value[3]+" "+value[5])  
+        list_with_summaries.append(value[7]+','+value[6]+','+ value[8])   
+
+    return list_with_dtstart_as_str, list_with_dtend_as_str, list_with_summaries
+
+
+#Convert date string lists into python datetime lists
+def strings_to_datetime(list_with_dtstart_as_str,list_with_dtend_as_str):
     
-    #date string list to python datetime list
-    date1 = [datetime.strptime(x,'%d.%m.%Y %H:%M') for x in list1]
-    print(date1)
-    print("-----------")
-    date2 = [datetime.strptime(x,'%d.%m.%Y %H:%M') for x in list2]
-   
-    #put data from the list into calendar dtstart   
+    list_with_dtstart_as_datetime = [datetime.strptime(x,'%d.%m.%Y %H:%M') for x in list_with_dtstart_as_str]
+    list_with_dtend_as_datetime = [datetime.strptime(x,'%d.%m.%Y %H:%M') for x in list_with_dtend_as_str]
+    return  list_with_dtstart_as_datetime, list_with_dtend_as_datetime
+
+#Function to create calendar  
+def create_calendar_with_events(start_time, end_time, SUMMARIS):
+
     cal = Calendar()
     cal.add('mothod','REQUEST')
-    #cal.add('prodid', '-//My calendar product//mxm.dk//')
+    cal.add('prodid', '-//My calendar product//mxm.dk//')
     cal.add('version', '2.0')
-   
-    for y in range(len(date1)) :    
-        
-        without_timezone = date1[y]
+    
+    #Loop for creationg events parsing data from 
+    for y in range(len(start_time)) :    
+
+    #Convert start time to start time with timezone
+        without_timezone = start_time[y]
         timezone = pytz.timezone("UTC")
         with_timezone = timezone.localize(without_timezone)
         print(with_timezone)
-        print(type(with_timezone))
-        without_time = date2[y]
+
+    #Convert end time to end time with timezone
+        without_time = end_time[y]
         timezone = pytz.timezone("UTC")
         with_time = timezone.localize(without_time)
-        
+
+     #Create events
         event = Event()      
         event.add('dtstart', with_timezone)
         event['dtstart'].to_ical()
@@ -59,11 +71,11 @@ def get_values(csv_data):
         event.add('dtstamp', with_timezone)
         event["DTSTAMP"].params.clear()
         event['uid'] = str(y)
-        event.add('summary', list3[y])
+        event.add('summary', SUMMARIS[y])
         event.add('priority', 5)
         cal.add_component(event)
-         
-    f = open('examp.ics', 'wb')
+    #Save events as ics file    
+    f = open('buene_events.ics', 'wb')
     f.write(cal.to_ical())
     f.close()
 
@@ -71,18 +83,16 @@ def get_values(csv_data):
 sg.theme('DarkPurple7') #choose a theme    
 sg.SetOptions(element_padding=(10, 10))      
 
-# menu definition     
-menu_def = [['File', ['Open', 'Exit'  ]],           
-                ['Help', 'About...'], ]      
+# Menu definition     
+menu_def = [['Help', 'About...']]  
 
-#layout creation 
+#Layout creation 
 layout = [ [sg.Menu(menu_def, )],
            [sg.Text('Application to parse data from csv to Nextcloud')],
            [sg.T('Enter the path to the SCV file'), sg.InputText(key='Input_SCV_path'), sg.FileBrowse(),
-            sg.B('Confirm', key='ConfirmPath')],
-           [sg.InputText(key='Input')], 
-           [sg.B('To Output'), sg.B('Popup')], 
-           [sg.Output(), sg.B('Confirm'), sg.B('Exit')], 
+            sg.B('Confirm', key='ConfirmPath')], 
+           [sg.B('Create ics file')], 
+           [sg.Output(), sg.B('Exit')], 
          ]      
 window = sg.Window('CSV to Nextcloud parser', layout)
 
@@ -96,15 +106,13 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Exit':      
         break            
     textInputs = values['Input'] 
-    if event == 'To Output':
-        get_values(scv_date)   
+    if event == 'Create ics file':
+        start_time_str, end_time_str, SUMMARIS = get_values(scv_date)
+        start_time, end_time = strings_to_datetime(start_time_str, end_time_str)
+        create_calendar_with_events(start_time, end_time, SUMMARIS)
         print(textInputs)
-    if event == 'Popup':  
-        sg.popup('you entered:', textInputs) #popup is a GUI equivalent of a print statement
         
     # Process menu choices  
     if event == 'About...':      
         sg.popup("1)For the application to work, click the 'Browse' Button and find....", "2) Click 'Confirm' button the path is correct")      
-    elif event == 'Open':      
-        filename = sg.popup_get_file('file to open', no_window=True)      
-        print(filename)      
+  
